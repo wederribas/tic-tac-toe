0292e6c7b6728b8fffea317ade679edc3a3d7cbe
wederribas <wederribas@gmail.com>
2020-04-26 23:41:48 -0700

    Add terser plugin for better minification

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,6 +1,11 @@
 const merge = require('webpack-merge')
+const TerserPlugin = require('terser-webpack-plugin')
 const baseConfig = require('./base.config')
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({ extractComments: false })],
+  },
 })
